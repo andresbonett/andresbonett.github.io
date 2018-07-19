ae34f3deffc90b66aa0cd87af4baa5ee7e90fb51
Andres Bonett <andresbonettm@gmail.com>
2018-07-19 01:51:36 -0500

    Agregando input de usuario

--- platzi/canvas/dibujo.js
+++ platzi/canvas/dibujo.js
@@ -1,5 +1,13 @@
-d = document.getElementById("dibujito");
-lienzo = d.getContext("2d");
+var t = document.getElementById("textbox");
+var colorbox = document.getElementById("colorbox");
+var b = document.getElementById("boton");
+
+b.addEventListener("click", clickBoton);
+
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var ancho = d.width;
+var colorcito;
 
 function dibujarLinea(c, x1, y1, x2, y2) {
 	lienzo.beginPath();
@@ -9,30 +17,36 @@ function dibujarLinea(c, x1, y1, x2, y2) {
 	lienzo.stroke();
 	lienzo.closePath();
 }
-// Torre Eiffel
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var nyi;
-var nxf = 300;
-var colorcito = "grey";
 
-for (l = 0; l < lineas; l++) {
-	yi = 10 * l;
-	xf = yi++;
-	nxf = nxf - 10;
-	dibujarLinea(colorcito,0,yi,xf,300);
-	dibujarLinea(colorcito,300,yi,xf,0);
-	dibujarLinea(colorcito,yi,300,300,nxf);
-	dibujarLinea(colorcito,0,nxf,yi,0);
+function clickBoton(){
+	
+	lienzo.clearRect(0, 0, d.width, d.height);
+	var lineas = parseInt(t.value);
+	var espacio = ancho / lineas;
+	var l = 0;
+	var yi, xf;
+	var nyi;
+	var nxf = 300;
+	if (colorcito === undefined){
+		colorcito = "black";
+	}
+	else{
+		colorcito = colorbox.value;
+	}
+	
+
+	for (l = 0; l < lineas; l++) {
+		yi = espacio * l;
+		xf = yi++;
+		nxf = nxf - espacio;
+		dibujarLinea(colorcito,0,yi,xf,300);
+		dibujarLinea(colorcito,300,yi,xf,0);
+		dibujarLinea(colorcito,yi,300,300,nxf);
+		dibujarLinea(colorcito,0,nxf,yi,0);
+	}
+
 }
-
 dibujarLinea("red",1,1,1,299);
 dibujarLinea("red",1,299,299,299);
 dibujarLinea("green",299,299,299,1);
 dibujarLinea("green",299,1,1,1);
-
-// dibujarLinea("blue",100,100,200,100);
-// dibujarLinea("red",200,100,200,200);
-// dibujarLinea("yellow",200,200,100,200);
-// dibujarLinea("green",100,200,100,100);
