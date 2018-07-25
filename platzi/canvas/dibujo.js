26e4ba1f92f254d13330a6b17089c0f223a73f3e
Andres Bonett <andresbonettm@gmail.com>
2018-07-25 00:53:54 -0500

    Mejoras en selecor de color

--- platzi/canvas/dibujo.js
+++ platzi/canvas/dibujo.js
@@ -1,13 +1,13 @@
 var t = document.getElementById("textbox");
 var colorbox = document.getElementById("colorbox");
 var b = document.getElementById("boton");
-
+var colorcito = "black";
 b.addEventListener("click", clickBoton);
 
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var ancho = d.width;
-var colorcito;
+
 
 function dibujarLinea(c, x1, y1, x2, y2) {
 	lienzo.beginPath();
@@ -21,19 +21,14 @@ function dibujarLinea(c, x1, y1, x2, y2) {
 function clickBoton(){
 	
 	lienzo.clearRect(0, 0, d.width, d.height);
+	colorcito = colorbox.value;
+	console.log(colorcito);
 	var lineas = parseInt(t.value);
 	var espacio = ancho / lineas;
 	var l = 0;
 	var yi, xf;
 	var nyi;
 	var nxf = 300;
-	if (colorcito === undefined){
-		colorcito = "black";
-	}
-	else{
-		colorcito = colorbox.value;
-	}
-	
 
 	for (l = 0; l < lineas; l++) {
 		yi = espacio * l;
@@ -44,9 +39,13 @@ function clickBoton(){
 		dibujarLinea(colorcito,yi,300,300,nxf);
 		dibujarLinea(colorcito,0,nxf,yi,0);
 	}
-
+	dibujarLinea("red",1,1,1,299);
+	dibujarLinea("red",1,299,299,299);
+	dibujarLinea("green",299,299,299,1);
+	dibujarLinea("green",299,1,1,1);
 }
 dibujarLinea("red",1,1,1,299);
 dibujarLinea("red",1,299,299,299);
 dibujarLinea("green",299,299,299,1);
 dibujarLinea("green",299,1,1,1);
+console.log(colorcito);
